5d95c3c6209592dec71cdcf2135fa17ef70aed69
John Polhill <JohnPolhill42@gmail.com>
2018-06-24 18:55:28 -0500

    Add working Mad Libs
    Add working Mad Libs

--- marugo/marugo.js
+++ marugo/marugo.js
@@ -14,6 +14,7 @@ var bunshou_map = [
 	['MONO','物'],
 	['MEISHI','名詞'],
 	['DOUSHI','動詞'],
+	['SURUDOUSHI', 'する動詞'],
 	['TEKEIDOUSHI','て形動詞'],
 	['JISHOKEIDOUSHI','辞書形動詞'],
 	['RENYOUKEI','連用形'],
@@ -23,20 +24,41 @@ var bunshou_map = [
 ]
 
 var current_story = sentences[Math.floor(Math.random()* sentences.length)];
+var next_word;
 
 var main = function() {
+	updateForm();
 	$('.mid-section').on('submit', "form", function(e) {
 		e.preventDefault();
+		var inputed_text = $("#input").val();
+		current_story = current_story.replace(next_word, inputed_text);
+		updateForm();
+		if (next_word === "") {
+			$('#story').text(current_story);
+			$('#submit').attr("disabled", "disabled");
+		}
 	})
+}
+
+var updateForm = function() {
+	next_word = findNextWord(current_story);
+	$("#gorui").text(getKanji(next_word) + ":");
+}
+
+var findNextWord = function(sentence) {
+	if (sentence.indexOf('_') === -1) return "";
+	sentence = sentence.substr(sentence.indexOf('_')+1, sentence.length - 1);
+	return "_" + sentence.substr(0, sentence.indexOf('_') + 1);
 }
 
 var getKanji = function(gorui) {
 	gorui = gorui.replace('_', '');
-	gorui = gorui.substr(gorui.indexOf('-') - 1, 3);
+	gorui = gorui.substr(0, gorui.indexOf('-'));
 	for (var i = 0; i < bunshou_map.length; i++) {
 		if (bunshou_map[i][0] === gorui)
 			return bunshou_map[i][1];
 	}
+    return "";
 }
 
 $(document).ready(main());
