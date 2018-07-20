06f8b3e61770bbfc6142a037763df412b77c39a9
joshpolhill <34665567+joshpolhill@users.noreply.github.com>
2018-07-19 20:24:42 -0500

    bleep
    bloop

--- snake.js
+++ snake.js
@@ -22,7 +22,42 @@ function Snake(x, y, size, color) {
 	this.y = y;
 	this.size = size;
 	this.color = color;
+	this.dir = 'R';
+	this.move = function() {
+		switch (this.dir) {
+			case 'L': 
+				this.x--;
+				break;
+			case 'U': 
+				this.y--;
+				break;
+			case 'R': 
+				this.x++;
+				break;
+			case 'D': 
+				this.y++;
+				break;
+		}
+	};
+	this.changeDir = function() {
+		if(keys.left)
+			this.dir = 'L';
+		else if(keys.up)
+			this.dir = 'U';
+		else if(keys.right)
+			this.dir = 'R';
+		else if(keys.down)
+			this.dir = 'D';
+	}
 }
+
+function eatApple() {
+	if(this.x === this.x && this.y === this.y) {
+		this.x = math.floor(math.random() * GS);
+		this.y = math.floor(math.random() * GS);
+	}
+}
+	
 
 Snake.prototype = Object.create(Square.prototype);
 
