0698c9ba36a1b311fa7a4800216c136fa53b8326
John Polhill <JohnPolhill42@gmail.com>
2018-07-24 22:12:21 -0500

    aaaaa
    aaaa

--- snake.js
+++ snake.js
@@ -24,7 +24,12 @@ function Snake(x, y, size, color) {
 	this.size = size;
 	this.color = color;
 	this.dir = 'R';
+	this.body = [new BodyPart(this.x - 1, this.y, this.size, this.color, this)];
 	this.move = function() {
+		for (var i = this.body.length - 1; i >= 0; i--) {
+			this.body[i].x = this.body[i].parent.x;
+			this.body[i].y = this.body[i].parent.y;
+		}
 		switch (this.dir) {
 			case 'L': 
 				this.x--;
@@ -44,6 +49,8 @@ function Snake(x, y, size, color) {
 		if(this.x === apple.x && this.y === apple.y) {
 			apple.x = Math.floor(Math.random() * ((myGameArea.canvas.width - this.size)/GS));
 			apple.y = Math.floor(Math.random() * ((myGameArea.canvas.height - this.size)/GS));
+			var tempBody = this.body[this.body.length -1];
+			this.body.push(new BodyPart(tempBody.x, tempBody.y, tempBody.size, tempBody.color, tempBody));
 		}
 	}
 	this.changeDir = function() {
@@ -61,15 +68,24 @@ function Snake(x, y, size, color) {
 		this.move();
 		this.eatApple();
 	};
+	this.render = function() {
+		var ctx = myGameArea.context;
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x * GS, this.y * GS, this.size * GS, this.size * GS);
+		this.body.forEach(function(part) {
+			part.render();
+		});
+	}
 }
 
 Snake.prototype = Object.create(Square.prototype);
 
-function BodyPart(x, y, size, color) {
+function BodyPart(x, y, size, color, parent) {
 	this.x = x;
 	this.y = y;
 	this.size = size;
 	this.color = color;
+	this.parent = parent;
 }
 
 BodyPart.prototype = Object.create(Square.prototype);
